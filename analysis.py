06a030e8178323c6bdebe4a83166acb40b9fbb8b
Denis Banu <denis@Deniss-MacBook-Pro-2.local>
2023-01-06 20:31:08 +0200

    Add topic in rdd.

--- analysis.py
+++ analysis.py
@@ -2,19 +2,27 @@ import findspark
 findspark.init()
 import re
 
-from pyspark import SparkConf, SparkContext
-from pyspark.sql import SQLContext
+from pyspark import SparkConf, SparkContext, RDD
+from pyspark.sql import SQLContext, Row
 from pyspark.sql.types import StructType, StructField, IntegerType, StringType, ArrayType
 from pyspark.sql.dataframe import DataFrame
 from pyspark.sql.functions import countDistinct, count, avg
 
 import topic_extractor
 from gensim.utils import simple_preprocess
-from gensim.models import CoherenceModel, Phrases
-from nltk.corpus import stopwords
+from gensim.models import CoherenceModel, Phrases, LdaMulticore
+from nltk.corpus import stopwords, wordnet
+import nltk
 import spacy
 import gensim.corpora as corpora
 import person_extractor
+
+# Uncomment it at first run
+# nltk.download('averaged_perceptron_tagger')
+# nltk.download('wordnet')
+# nltk.download('stopwords')
+
+from nltk.stem import WordNetLemmatizer
 
 def init():
     global ner
@@ -47,12 +55,8 @@ def read_dataset(sc: SQLContext):
 
     return ds
 
-def get_topic(corpus, id2word):
-    topic_extractor.get_topic_modeling_result(corpus, id2word)
-
-
 import gensim.models
-def make_grams(data_words):
+def make_bigrams(data_words):
     # Build the bigram and trigram models
     bigram = gensim.models.Phrases(data_words, min_count=5, threshold=100)
 
@@ -61,55 +65,111 @@ def make_grams(data_words):
 
     return bigram_mod
 
-def lemmatize_sentence(sentence, nlp):
-    allowed_postags=['NOUN', 'ADJ', 'VERB', 'ADV']
-    doc = nlp(" ".join(sentence))
-    return [token.lemma_ for token in doc if token.pos_ in allowed_postags]
+def right_tag(treebank_tag):
+    if treebank_tag.startswith('J'):
+        return True
+    elif treebank_tag.startswith('V'):
+        return True
+    elif treebank_tag.startswith('N'):
+        return True
+    elif treebank_tag.startswith('R'):
+        return True
+    else:
+        return False
 
+def get_wordnet_pos(treebank_tag):
+    if treebank_tag.startswith('J'):
+        return wordnet.ADJ
+    elif treebank_tag.startswith('V'):
+        return wordnet.VERB
+    elif treebank_tag.startswith('N'):
+        return wordnet.NOUN
+    elif treebank_tag.startswith('R'):
+        return wordnet.ADV
+    else:
+        return wordnet.NOUN
 
-def add_text_topic(ds: DataFrame):
-    # here we will add the implementation for subject finder
-    contents = ds.rdd.map(lambda row: row['content']) \
-        .map(lambda content: re.sub('[,\.!?]', '', content)) \
-        .map(lambda content: content.lower()) \
-        .map(lambda sentence: simple_preprocess(str(sentence), deacc=True)) \
-        .cache()
-    
-    data_words = list(contents.collect())
+def lemmatization(doc):
+    print("AAA")
+    lemmatizer = WordNetLemmatizer()
+    a = [lemmatizer.lemmatize(word, get_wordnet_pos(tag)) for word, tag in nltk.pos_tag(doc) if right_tag(tag)]
+    print(a)
+    return a
 
-    bigram_mod = make_grams(data_words)
+def preprocess_content(row):
+    content = row['pc']
+    content = re.sub('[,\.!?]', '', content).lower()
+    row['pc'] = simple_preprocess(content, deacc=True)
+    return row
 
+def remove_stopwords(row, stop_words):
+    content = row['pc']
+    row['pc'] = [word for word in content if word not in stop_words]
+    return row
+
+def add_bag_of_words(row, id2word):
+    row['pc'] = id2word.doc2bow(row['pc'])
+    return row
+
+def add_preprocesed_content(row: dict):
+    row["pc"] = row["content"]
+    return row
+
+def add_topic_in_row(row, topic) -> Row:
+    row["topic"] = topic
+    row.pop("pc", None)
+    return Row(**row)
+
+def get_topic(model: LdaMulticore, text):
+    topics = model.get_document_topics(text)
+    mx = topics[0][1]
+    best_topic = topics[0][0]
+    for t in topics:
+        if t[1] > mx:
+            mx = t[1]
+            best_topic = t[0]
+    return best_topic
+
+def add_text_topic(ds: DataFrame) -> tuple[DataFrame, LdaMulticore]:
     stop_words = stopwords.words('english')
     stop_words.extend(['from', 'subject', 're', 'told', 'make', 'edu', 'use',
                        'could', 'many', 'said', 'mr', 'bbc', 'also', 'ms', 'one',
                        'two', 'three', 'year', 'would', 'says', 'government',
                        'people', 'minister', 'may', 'need', 'see'])
 
-    nlp = spacy.load("en_core_web_sm", disable=['parser', 'ner'])
-
-    texts = contents.map(lambda sentence: [word for word in sentence if word not in stop_words]) \
-        .map(lambda sentence: bigram_mod[sentence]) \
-        .map(lambda sentence: lemmatize_sentence(sentence, nlp)) \
+    # here we will add the implementation for subject finder
+    texts = ds.rdd.map(lambda row: add_preprocesed_content(row.asDict())) \
+        .map(lambda row: preprocess_content(row)) \
+        .map(lambda row: remove_stopwords(row, stop_words)) \
         .cache()
+        # To do add lemmatization
+        # .map(lambda sentence: lemmatization(sentence)) \
     
-    data_lemmatized = list(texts.collect())
+    data_lemmatized = list(texts.map(lambda row: row['pc']).collect())
     id2word = corpora.Dictionary(data_lemmatized)
+    texts = texts.map(lambda row: add_bag_of_words(row, id2word)).cache()
+    corpus = list(texts.map(lambda row: row['pc']).collect())
+    model = topic_extractor.train_model(corpus, id2word)
 
-    corpus = list(texts.map(lambda text: id2word.doc2bow(text)).collect())
-
-    get_topic(corpus, id2word)
+    return texts.map(lambda row: add_topic_in_row(row, get_topic(model, row['pc']))).toDF(), model, corpus
 
 def add_person_in_row(row: dict):
     persons = person_extractor.get_person_names(row["content"], ner)
     row["persons"] = persons
     return row
 
-def add_persons(ds: DataFrame):
-    ds.rdd.map(lambda row: add_person_in_row(row.asDict())).foreach(lambda row: print(row["title"], row["persons"]))
+def add_persons(ds: DataFrame) -> DataFrame:
+    ds.rdd.map(lambda row: add_person_in_row(row.asDict())).toDF
+        
 
 if __name__ == "__main__":
     init()
     sc = init_spark()
+    # Here we use only a sample of 10 texts 
     ds = read_dataset(sc).limit(10).cache()
-    # add_text_topic(ds)
-    add_persons(ds)
+    ds_topic, topic_model, corpus = add_text_topic(ds)
+    ds_topic.rdd.foreach(lambda row: print(row["title"], "=>" , row["topic"], topic_model.print_topic(row["topic"], 10)))
+    
+    
+    ds_person = add_persons(ds)
+    ds_person.rdd.foreach(lambda row: print(row["title"], row["persons"]))
--- topic_extractor.py
+++ topic_extractor.py
@@ -2,7 +2,7 @@ import pandas as pd
 import spacy
 import re
 import nltk
-# nltk.download('stopwords')
+
 from nltk.corpus import stopwords
 import gensim
 import gensim.corpora as corpora
@@ -11,62 +11,9 @@ from gensim.models import CoherenceModel, Phrases
 from pprint import pprint
 
 
-def sent_to_words(sentences):
-    for sentence in sentences:
-        yield gensim.utils.simple_preprocess(str(sentence), deacc=True)
-
-
-def remove_stopwords(local_texts):
-    stop_words = stopwords.words('english')
-    stop_words.extend(['from', 'subject', 're', 'told', 'make', 'edu', 'use',
-                       'could', 'many', 'said', 'mr', 'bbc', 'also', 'ms', 'one',
-                       'two', 'three', 'year', 'would', 'says', 'government',
-                       'people', 'minister', 'may', 'need', 'see'])
-    return [[word for word in simple_preprocess(str(doc))
-             if word not in stop_words] for doc in local_texts]
-
-
-# def make_bigrams(local_texts):
-#     return [bigram_mod[doc] for doc in local_texts]
-
-
-# def make_trigrams(local_texts):
-#     return [trigram_mod[bigram_mod[doc]] for doc in local_texts]
-
-
-def lemmatization(local_texts, allowed_postags=None):
-    """https://spacy.io/api/annotation"""
-
-    nlp = spacy.load("en_core_web_sm", disable=['parser', 'ner'])
-    if allowed_postags is None:
-        allowed_postags = ['NOUN', 'ADJ', 'VERB', 'ADV']
-    texts_out = []
-    for sent in local_texts:
-        doc = nlp(" ".join(sent))
-        texts_out.append([token.lemma_ for token in doc if token.pos_ in allowed_postags])
-    return texts_out
-
-
-# supporting function
-def compute_coherence_values(corpus, dictionary, k, a, b):
-    lda_model = gensim.models.LdaMulticore(corpus=corpus,
-                                           id2word=dictionary,
-                                           num_topics=k,
-                                           random_state=100,
-                                           chunksize=100,
-                                           passes=10,
-                                           alpha=a,
-                                           eta=b)
-
-    coherence_model_lda = CoherenceModel(model=lda_model, texts=data_lemmatized, dictionary=id2word, coherence='c_v')
-
-    return coherence_model_lda.get_coherence()
-
-
-def get_topic_modeling_result(corpus, id2word):
+def train_model(corpus, id2word):
     # Number of topics
     num_topics = 30
-    num_words = 30
     # Build LDA model
     # Build LDA model
     lda_model = gensim.models.LdaMulticore(corpus=corpus,
@@ -76,86 +23,5 @@ def get_topic_modeling_result(corpus, id2word):
                                            chunksize=100,
                                            passes=10,
                                            per_word_topics=True)
-    # Print the Keyword in the "num_topics" topics
 
-    # doc_lda = lda_model[corpus]
-
-    # topics_w_words = lda_model.print_topics(num_topics=num_topics, num_words=num_words)
-    pprint(lda_model.print_topics(num_topics=num_topics, num_words=num_words))
-    return lda_model.print_topics(num_topics=num_topics, num_words=num_words)
-
-
-# if __name__ == '__main__':
-#     data = pd.read_csv('./dataset.csv')
-#     data = data.drop(columns=['crt', 'title', 'published_date', 'authors', 'description', 'section', 'link'], axis=1)
-#     # Remove punctuation
-#     data['data_text_processed'] = data['content'].map(lambda x: re.sub('[,\.!?]', '', x))
-
-#     # Convert the titles to lowercase
-#     data['data_text_processed'] = data['data_text_processed'].map(lambda x: x.lower())
-
-
-
-#     data_list_of_sent = data.data_text_processed.values.tolist()
-#     data_words = list(sent_to_words(data_list_of_sent))
-
-#     # Build the bigram and trigram models
-#     bigram = gensim.models.Phrases(data_words, min_count=5, threshold=100)
-#     trigram = gensim.models.Phrases(bigram[data_words], threshold=100)
-
-#     # Faster way to get a sentence clubbed as a trigram/bigram
-#     bigram_mod = gensim.models.phrases.Phraser(bigram)
-#     trigram_mod = gensim.models.phrases.Phraser(trigram)
-
-#     # Remove Stop Words
-#     data_words_nostops = remove_stopwords(data_words)
-
-#     # Form Bigrams
-#     data_words_bigrams = make_bigrams(data_words_nostops)
-
-#     # Do lemmatization keeping only noun, adj, vb, adv
-#     data_lemmatized = lemmatization(data_words_bigrams, allowed_postags=['NOUN', 'ADJ', 'VERB', 'ADV'])
-
-#     # Create Dictionary
-#     id2word = corpora.Dictionary(data_lemmatized)
-#     # Create Corpus
-#     texts = data_lemmatized
-#     # Term Document Frequency
-#     corpus = [id2word.doc2bow(text) for text in texts]
-
-
-#     # Number of topics
-#     num_topics = 30
-#     num_words = 30
-#     # Build LDA model
-#     # Build LDA model
-#     lda_model = gensim.models.LdaMulticore(corpus=corpus,
-#                                            id2word=id2word,
-#                                            num_topics=num_topics,
-#                                            random_state=100,
-#                                            chunksize=100,
-#                                            passes=10,
-#                                            per_word_topics=True)
-#     # Print the Keyword in the "num_topics" topics
-
-#     doc_lda = lda_model[corpus]
-
-#     topics_w_words = lda_model.print_topics(num_topics=num_topics, num_words=num_words)
-#     pprint(lda_model.print_topics(num_topics=num_topics, num_words=num_words))
-
-#     final_sentence_words = []
-#     final_sentences = []
-
-#     for topic in topics_w_words:
-#         sentence_words = str(topic[1]).replace("\"", "").split("+")
-#         index = 0
-#         final_sentence_words = []
-#         for sentence_word in sentence_words:
-#             sentence_words[index] = sentence_word.split("*")
-#             index += 1
-#         for nb, word in sentence_words:
-#             final_sentence_words.append(word.strip())
-
-#         final_sentences.append(final_sentence_words.copy())
-
-#     print(final_sentences)
+    return lda_model
